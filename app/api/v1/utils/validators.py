36c2fab17488200b1c7abfc59f550761b7886a02
root <root@localhost.localdomain>
2019-01-15 13:59:25 +0300

    [bug #163073842] fix validators not working correctly bug

--- app/api/v1/utils/validators.py
+++ app/api/v1/utils/validators.py
@@ -1,83 +1,75 @@
 import sys
 import re
 
+class Validator:
 
-class Validator():
-
-    def __init__(self, data):
+    def __init__(self, data={}):
         self.data = data
 
     def check_fields(self):
         for key, value in self.data.items():
             if not value:
-                resp = {
-                    "message": '{} field Cannot be empty'.format(key)
-                }
-                return resp
+                resp =  '{} field Cannot be empty'.format(key)
+                break
             else:
-                resp = {
-                    "message": True
-                }
-                return resp
+                resp = True
+
+        return resp
+
 
     def check_email(self):
         if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", self.data['email']):
-            resp = {
-                "message": 'Please enter a valid email'
-            }
+            resp =  'Please enter a valid email'
+
             return resp
+
         else:
-            resp = {
-                "message": True
-            }
+            resp = True
+
             return resp
+
 
     def check_password(self):
         password = self.data['password']
 
         if len(password) < 6:
-            resp = {
-                "message": 'password field must be more than 6 characters'
-            }
+            resp = 'password field must be more than 6 characters'
+
             return resp
+
 
         elif not re.search("[a-z]", password):
-            resp = {
-                "message": 'password field must contain atleast 1 lowercase letter'
-            }
+            resp = 'password field must contain atleast 1 lowercase letter'
+
             return resp
+
 
         elif not re.search("[A-Z]", password):
-            resp = {
-                "message": 'password field must contain atleast 1 uppercase letter'
-            }
+            resp = 'password field must contain atleast 1 uppercase letter'
+
             return resp
+
 
         elif not re.search("[0-9]", password):
-            resp = {
-                "message": 'password field must contain atleast 1 digit'
-            }
+            resp = 'password field must contain atleast 1 digit'
+
             return resp
 
+
         elif not re.search("[_@$#!*]", password):
-            resp = {
-                "message": 'password field must contain one of the following characters _ @ $ # ! *'
-            }
+            resp = 'password field must contain one of the following characters _ @ $ # ! *'
             return resp
+
         else:
-            resp = {
-                "message": True
-            }
+            resp = True
             return resp
+
 
     def pass_match(self):
         if self.data['password'] != self.data['confirm_password']:
-            resp = {
-                "message": "password does not match"
-            }
+            resp = "password does not match"
             return resp
+
         else:
-            resp = {
-                "message": True
-            }
+            resp = True
             return resp
--- app/api/v1/views/auth.py
+++ app/api/v1/views/auth.py
@@ -12,31 +12,45 @@ class User(Resource):
 
     def post(self):
         data = request.get_json()
-        validate = Validator(data)
-        firstname = data['firstname']
-        lastname = data['lastname']
-        othername = data['othername']
-        email = data['email']
-        phoneNumber = data['phoneNumber']
-        username = data['username']
-        registered = data['registered']
-        isAdmin = data['isAdmin']
-        password = data['password']
-        confirm_password = data['confirm_password']
 
-        if password == confirm_password:
+        validate = Validator(data)
+        check_fields = validate.check_fields()
+        check_password = validate.check_password()
+        check_email = validate.check_email()
+        check_password_match= validate.pass_match()
+
+        if check_fields != True:
+            return make_response(
+                jsonify({"error": check_fields, "status": 400}), 400)
+
+        elif check_email != True:
+            return make_response(
+                jsonify({"error": check_email, "status": 400}), 400)
+        elif check_password != True:
+            return make_response(
+                jsonify({"error": check_password, "status": 400}), 400)
+        elif check_password_match != True:
+            return make_response(
+                jsonify({"error": check_password_match, "status": 400}), 400)
+
+        else:
+
+            firstname = data['firstname']
+            lastname = data['lastname']
+            othername = data['othername']
+            email = data['email']
+            phoneNumber = data['phoneNumber']
+            username = data['username']
+            registered = data['registered']
+            isAdmin = data['isAdmin']
+            password = data['password']
+            confirm_password = data['confirm_password']
+
             _b_save = UserModel(firstname, lastname, othername,
                                 phoneNumber, username, email, password)
 
             resp = _b_save.save()
-            data_list = []
-            data_list.append(resp)
-            return make_response(jsonify({'data': data_list, "status": 201}), 201)
-
-        else:
-            return make_response(jsonify({"error": "Passwords don't match",
-                                          "status": 400}), 400)
-
+            return make_response(jsonify({'message': resp, "status": 201}), 201)
 
 class UserLogin(Resource):
     def post(self):
@@ -44,18 +58,34 @@ class UserLogin(Resource):
         email = data['email']
         password = data['password']
 
-        user = UserModel().return_data(email=email)
-        print(user, file=sys.stdout)
-        data_list = []
+        validate = Validator(data)
 
-        if user is not None:
-            if user['password'] == password:
+        check_fields = validate.check_fields()
+        check_password = validate.check_password()
+        check_email = validate.check_email()
 
-                return make_response(jsonify({"message": "Successfully Logged In",
-                                              "status": 200}), 200)
-            else:
-                return make_response(jsonify({"error": "wrong email or Password",
-                                              "status": 401}), 401)
+        if check_fields != True:
+            return make_response(
+                jsonify({"error": check_fields, "status": 400}), 400)
+
+        elif check_email != True:
+            return make_response(
+                jsonify({"error": check_email, "status": 400}), 400)
+
         else:
-            return make_response(jsonify({"error": "User does not exist",
-                                          "status": 404}), 404)
+            try:
+                user = UserModel().return_data(email=email)
+                print(user, file=sys.stdout)
+                data_list = []
+
+                if user['password'] == password:
+
+                    return make_response(jsonify({"message": "Successfully Logged In",
+                                                  "status": 200}), 200)
+                else:
+                    return make_response(jsonify({"error": "wrong email or Password",
+                                                  "status": 401}), 401)
+
+            except:
+                return make_response(jsonify({"error": "User does not exist",
+                                              "status": 404}), 404)
